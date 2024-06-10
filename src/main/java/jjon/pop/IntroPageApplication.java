6700f1944ed28417ba1167d9a2ac0aaaae86798b
owencity <skyko6530@gmail.com>
2024-06-11 00:02:46 +0900

    ready

---
+++ src/main/java/jjon/pop/IntroPageApplication.java
@@ -0,0 +1,13 @@
+package jjon.pop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroPageApplication.class, args);
+	}
+
+}
