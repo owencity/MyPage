79f79b86908c81f1ead906c396644bf6b9dc17ea
owencity <skyko6530@gmail.com>
2024-06-19 22:08:42 +0900

    mapperinterface void -> boolean type change, Service class logic add

--- src/main/java/jjon/BoardMapper.java
+++ src/main/java/jjon/BoardMapper.java
@@ -1,6 +1,20 @@
 package jjon;
 
-@mapper
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface BoardMapper {
+	
+	public List<BoardDto> getList(BoardDto boardDto);
+	public boolean boardInsert(BoardDto boardDto);
+	public void boardDelete(BoardDto boardDTo);
+	public void boardUpdate(BoardDto boardDto);
+	public int boardSearch(int id);
+	public int boardCount(int count);
+
+	
+	
 
 }
--- src/main/java/jjon/BoardService.java
+++ src/main/java/jjon/BoardService.java
@@ -1,17 +1,32 @@
 package jjon;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Service
 public class BoardService {
 	
 	private final BoardMapper boardMapper;
+	private final BoardDto boardDto;
 	
-	public BoardService(BoardMapper boardMapper) {
+	public BoardService(BoardMapper boardMapper, BoardDto boardDto) {
 		this.boardMapper = boardMapper;
+		this.boardDto = boardDto;
 	}
 	
-	public List<Board> getList() {
+	public List<BoardDto> getList() {
+		List<BoardDto> list = boardMapper.getList(boardDto);
+		return list;
+	}
+	
+	public boolean boardInsert() {
+		boolean result = boardMapper.boardInsert(boardDto);
+		
+		return result;
 		
 		
 	}
